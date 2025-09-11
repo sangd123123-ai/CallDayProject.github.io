419fa21e6c66ef11ec6394c9b956570665e5bf49
eternus24 <id8558@gmail.com>
2025-09-11 18:15:09 +0900

    controller

--- CallDayProject/src/main/java/com/cdp/health/HealthController.java
+++ CallDayProject/src/main/java/com/cdp/health/HealthController.java
@@ -1,15 +1,13 @@
 package com.cdp.health;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cdp.health.dao.HealthDAO;
+import com.cdp.health.util.MyUtil;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +15,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HealthController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
+	@Autowired
+	@Qualifier("healthDAO")
+	HealthDAO dao;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	MyUtil myUtil;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home() {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		return "main";
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
 	}
 	
 }
